a6293a686c19e515e983a9af77955f161d05dd2e
Derek Dattero <68528944+ddattero@users.noreply.github.com>
2022-11-08 16:54:14 -0500

    first commit

---
+++ twit.py
@@ -0,0 +1,67 @@
+import tweepy
+
+import config
+
+import datetime
+
+from time import sleep
+
+
+twclient = tweepy.Client(
+    #consumer_key=config.API_KEY, consumer_secret=config.API_SECRET, access_token=config.ACCESS_TOKEN, access_token_secret=config.ACCESS_SECRET
+    bearer_token=config.BEARER
+)
+
+search_terms = [
+    'mental health', 
+    'depression',
+    'depressed' 
+    'anxiety',
+    'anxious' 
+    'ADHD', 
+    'attention deficit'
+    'OCD',
+    'obsessive-compulsive disorder',
+    'PTSD',
+    'posttraumatic stress disorder',
+    'trauma',
+    'bipolar',
+    'autism',
+    'schizophrenia',
+    'eating disorder',
+    'anorexia',
+    'bulimia',
+    'psychosis',
+    'psychologist',
+    'psychotherapist',
+    'psychiatrist',
+    'psychotherapy',
+    'depressant'
+]
+
+query = '('
+
+for i in range(len(search_terms)):
+    query = query + search_terms[i]
+    if i < len(search_terms) - 1:
+        query = query + ' OR '
+    else:
+        query = query + ')'
+
+query = query + ' -is:retweet'
+
+file_name = 'mental_health_tweets.csv'
+
+with open(file_name, 'a+') as filehandler:
+    filehandler.write(f'tweet\ttime')
+
+
+while True:
+    with open(file_name, 'a+') as filehandler:
+        mhsample = twclient.search_recent_tweets(query=query)
+        time = datetime.datetime.now()
+        for i in mhsample.data:
+            i = i.text.replace('\n', '')
+            filehandler.write(f'\n\"{i}\"\t{time}')
+    sleep(120)
+
---
+++ twitcount.py
@@ -0,0 +1,72 @@
+import tweepy
+
+import config
+
+import datetime
+
+from time import sleep
+
+
+twclient = tweepy.Client(
+    #consumer_key=config.API_KEY, consumer_secret=config.API_SECRET, access_token=config.ACCESS_TOKEN, access_token_secret=config.ACCESS_SECRET
+    bearer_token=config.BEARER
+)
+
+search_terms = [
+    'mental health', 
+    'depression',
+    'depressed' 
+    'anxiety',
+    'anxious' 
+    'ADHD', 
+    'attention deficit'
+    'OCD',
+    'obsessive-compulsive disorder',
+    'PTSD',
+    'posttraumatic stress disorder',
+    'trauma',
+    'bipolar',
+    'autism',
+    'schizophrenia',
+    'eating disorder',
+    'anorexia',
+    'bulimia',
+    'psychosis',
+    'psychologist',
+    'psychotherapist',
+    'psychiatrist',
+    'psychotherapy',
+    'depressant'
+]
+
+query = '('
+
+for i in range(len(search_terms)):
+    query = query + search_terms[i]
+    if i < len(search_terms) - 1:
+        query = query + ' OR '
+    else:
+        query = query + ')'
+
+query = query + ' -is:retweet'
+
+file_name = 'mental_health_tweet_count.csv'
+
+with open(file_name, 'a+') as filehandler:
+    filehandler.write(f'timestamp\tcount')
+
+
+today = datetime.date.today()
+today = datetime.datetime(year=today.year, month=today.month, day=today.day)
+yesterday = today - datetime.timedelta(1)
+
+
+
+while True:
+    with open(file_name, 'a+') as filehandler:
+        counts = twclient.get_recent_tweets_count(query=query, end_time=today, start_time=yesterday, granularity='hour')
+        for i in counts.data:
+            start = i['start']
+            count = i['tweet_count']
+            filehandler.write(f'\n\"{start}\"\t{count}')
+    sleep(86400)
