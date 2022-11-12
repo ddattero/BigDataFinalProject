55e2d1f0db960bfc4cbdcff10473ec2ba8620c8f
Derek Dattero <68528944+ddattero@users.noreply.github.com>
2022-11-12 13:14:05 -0500

    fixed bot code to be more resilient to errors

--- twit.py
+++ twit.py
@@ -57,11 +57,14 @@ with open(file_name, 'a+') as filehandler:
 
 
 while True:
-    with open(file_name, 'a+') as filehandler:
-        mhsample = twclient.search_recent_tweets(query=query)
-        time = datetime.datetime.now()
-        for i in mhsample.data:
-            i = i.text.replace('\n', '')
-            filehandler.write(f'\n\"{i}\"\t{time}')
+    try:
+        with open(file_name, 'a+') as filehandler:
+            mhsample = twclient.search_recent_tweets(query=query)
+            time = datetime.datetime.now()
+            for i in mhsample.data:
+                i = i.text.replace('\n', '')
+                filehandler.write(f'\n\"{i}\"\t{time}')
+    except:
+        pass
     sleep(120)
 
